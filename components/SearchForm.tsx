cc0c970371429ff228fee8f0529e6aa3a8bb7113
Moustapha Amroune <amroune.moustapha@gmail.com>
2026-02-19 14:47:00 +0100

    feat : submit username from search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,9 +1,22 @@
+"use client";
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [username, setUsername] = useState("");
+
   return (
-    <form>
+    <form
+      className="flex gap-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(username);
+      }}
+    >
       <input
         placeholder="GitHub username"
         className="border px-3 py-2 rounded"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <button className="border px-3 py-2 rounded">Generate</button>
     </form>
